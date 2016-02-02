cc02bafea2e076a1eca2bbaa920b9b884ff87668
GersonMoghtader <gersonmoghtader@hotmail.com>
2016-02-02 13:36:33 -0500

    actualizando

--- clase01/PedidoApp/src/pe/ghmv/pedidoapp/view/PedidoView.java
+++ clase01/PedidoApp/src/pe/ghmv/pedidoapp/view/PedidoView.java
@@ -19,6 +19,7 @@ public class PedidoView extends javax.swing.JFrame {
      */
     public PedidoView() {
         initComponents();
+        this.setLocationRelativeTo(null);//centrar formulario
     }
 
     /**
@@ -32,7 +33,7 @@ public class PedidoView extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         txtImporte = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        btnProcesar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("COMPRAS");
@@ -41,11 +42,11 @@ public class PedidoView extends javax.swing.JFrame {
 
         txtImporte.setToolTipText("");
 
-        jButton1.setText("Procesar");
-        jButton1.setToolTipText("");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btnProcesar.setText("Procesar");
+        btnProcesar.setToolTipText("");
+        btnProcesar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btnProcesarActionPerformed(evt);
             }
         });
 
@@ -58,7 +59,7 @@ public class PedidoView extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(10, 10, 10)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(btnProcesar, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -73,14 +74,14 @@ public class PedidoView extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(txtImporte, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnProcesar, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btnProcesarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProcesarActionPerformed
 //variables
 double importe,impuesto,total;
 PedidoService service = new PedidoService();
@@ -99,7 +100,7 @@ repo+="total: "+ total + "\n";
 JOptionPane.showMessageDialog(rootPane, repo);
 
 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnProcesarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -137,7 +138,7 @@ JOptionPane.showMessageDialog(rootPane, repo);
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton btnProcesar;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField txtImporte;
     // End of variables declaration//GEN-END:variables
